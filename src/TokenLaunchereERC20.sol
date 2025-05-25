10dbc0ebff4c66d7ff4607ebcfbba311d22ce802
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-05-25 19:36:05 +0100

    omo all we can say is omo oo

--- src/TokenLaunchereERC20.sol
+++ src/TokenLaunchereERC20.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.20;
 import "./Token.sol";
 
 contract TokenLauncher {
+    struct TokenDetails {
+        string name;
+        string symbol;
+        uint8 decimals;
+    }
+
     address[] public allTokens;
+    mapping(address => TokenDetails) public tokenDetailsMap;
 
     event TokenCreated(address tokenAddress, string name, string symbol, uint8 decimals);
 
@@ -17,12 +24,19 @@ contract TokenLauncher {
         address allPurposeVerifier
     ) external returns (address tokenAddress) {
         MyEncryptedToken token = new MyEncryptedToken(name, symbol, decimals, registrar, allPurposeVerifier);
-        allTokens.push(address(token));
-        emit TokenCreated(address(token), name, symbol, decimals);
-        return address(token);
+        address newTokenAddress = address(token);
+        allTokens.push(newTokenAddress);
+        tokenDetailsMap[newTokenAddress] = TokenDetails(name, symbol, decimals);
+        emit TokenCreated(newTokenAddress, name, symbol, decimals);
+        return newTokenAddress;
     }
 
     function getAllTokens() external view returns (address[] memory) {
         return allTokens;
+    }
+
+    function getTokenDetails(address tokenAddress) external view returns (string memory name, string memory symbol, uint8 decimals) {
+        TokenDetails memory details = tokenDetailsMap[tokenAddress];
+        return (details.name, details.symbol, details.decimals);
     }
 }
